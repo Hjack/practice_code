fa2bd850c92fe32e1f60f40527a5d601a018f124
hakimu jackson <hakimuj@gmail.com>
2013-08-24 13:18:48 -0700

    Remove Random from line 4

--- array_20.rb
+++ array_20.rb
@@ -1,7 +1,7 @@
 #Create an array of twenty random number
 #Create a new array from the random twenty that do not have any duplicate numbers.
 
-array = 20.times.map{Random.rand(50)}
+array = 20.times.map{rand(50)}
 new_array = []
 array.each do |n|
 	new_array << n unless new_array.include?(n)
